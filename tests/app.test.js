3e2cbd956e18e667d0b0db3d6ec6f889995da6dd
Nicolas Chavez <nicodizel@gmail.com>
2025-03-31 20:22:24 -0300

    Adding correction to the test.

--- tests/app.test.js
+++ tests/app.test.js
@@ -6,7 +6,7 @@ describe('API Tests', () => {
   it('should return a list of users', async () => {
     const res = await request(app).get('/users');
     expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveLength(3);
+    expect(res.body).toHaveLength(2);
   });
 
   it('should return a single user', async () => {
